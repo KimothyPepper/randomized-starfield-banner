8b1e1770ead1a5b9fe7ce73abf311c8f554b6975
KimothyPepper <kim-pep@hotmail.com>
2016-04-01 10:36:35 -0400

    Added content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-var $body =$('body');
+var $body = $('body');
+var $star;
 
 for (i = 0; i < 75; i++) {
-  var $star = $('<div>')
-
+  $star = $('<div>');
   $star.addClass('star');
   $star.css({
     'top': Math.random() * (document.documentElement.clientHeight - 100),
     'left': Math.random() * (document.documentElement.clientWidth - 100),
+    'transform': Math.random() * (document.documentElement.clientRotation - 100),
     'opacity': Math.random()
   });
   $body.append($star);
